11b1cd086c22277939043b25b0d88564f0db3798
Coko_33 <nicolasplacido@gmail.com>
2022-07-09 10:41:59 -0300

    feat randomize

---
+++ main.js
@@ -0,0 +1,9 @@
+let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+let numContainer = document.querySelector("#numero");
+
+/* numeros.forEach((e) => {
+  setInterval(() => {
+    numContainer.innerHTML = e;
+  }, 2000);
+}); */
