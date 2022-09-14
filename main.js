dedeaec24723be85cc43b1329123487ab1b241f2
Coko33 <nicolasplacido@gmail.com>
2022-09-14 18:30:34 -0300

    feat randomize 2

--- main.js
+++ main.js
@@ -1,9 +1,42 @@
+const MAX = 4;
+const MIN = 3;
+
 let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
 let numContainer = document.querySelector("#numero");
+let tiempo = 0;
+let tiempoRandom;
+let intervalo = 10;
 
-/* numeros.forEach((e) => {
+/* 
+t is current time
+b is start value
+c is change in value
+d is duration 
+*/
+function easeInOutQuad(t, b, c, d) {
+  t /= d / 2;
+  if (t < 1) return (c / 2) * t * t + b;
+  t--;
+  return (-c / 2) * (t * (t - 2) - 1) + b;
+}
+
+function numeroRandom(MIN, MAX) {
+  return Math.random() * (MAX - MIN) + MIN;
+}
+
+function iniciar() {
+  intervalo = 100;
+  tiempoRandom = numeroRandom(MIN, MAX);
+
   setInterval(() => {
-    numContainer.innerHTML = e;
-  }, 2000);
-}); */
+    intervalo = intervalo + 10;
+    console.log(numeros[Math.floor(Math.random() * numeros.length)]);
+    console.log(intervalo);
+  }, intervalo);
+
+  /*   numeros.forEach((e) => {
+    setInterval(() => {
+      console.log(e);
+    }, 2000);
+  }); */
+}
