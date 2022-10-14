40fb2719d13c05cc2cea991c471c31b4679ccf20
Coko33 <nicolasplacido@gmail.com>
2022-10-14 17:36:43 -0300

    feat randomize 4}

---
+++ musas.js
@@ -0,0 +1,123 @@
+var MUSAS = {
+  musas: [
+    {
+      id: 1,
+      nombre: "Bartolina Sisa",
+      descripcion:
+        "Bartolina Sisa Vargas. Fue declarada como una heroína nacional aimara según la ley n. 3102, 15 de julio de 2005 en Bolivia. Durante la colonia luchó junto con su esposo Julián Apaza conocido como Tupac Katari, organizó varios campamentos militares y participó del cerco a La Paz.",
+      linkImg: "https://musas.s3.sa-east-1.amazonaws.com/1.png",
+    },
+    {
+      id: 2,
+      nombre: "Gabo Ferro",
+      descripcion:
+        "Gabriel Fernando «Gabo» Ferro (Buenos Aires, 6 de noviembre de 1965 - 8 de octubre de 2020) fue un cantante de rock, compositor, cantautor, poeta, historiador y docente universitario argentino.",
+      linkImg: "https://musas.s3.sa-east-1.amazonaws.com/2.png",
+    },
+    {
+      id: 3,
+      nombre: "Horacio González",
+      descripcion:
+        "Horacio Luis González (Buenos Aires, 1 de febrero de 1944, 22 de junio de 2021) fue un sociólogo, filósofo, pensador, intelectual, historiador, investigador, escritor y profesor argentino.",
+      linkImg: "https://musas.s3.sa-east-1.amazonaws.com/3.png",
+    },
+    {
+      id: 4,
+      nombre: "Isabel Sarli",
+      descripcion:
+        "Hilda Isabel Gorrindo Sarli (Concordia, 9 de julio de 1935-San Isidro, 25 de junio de 2019), popularmente conocida como «la Coca Sarli», fue una actriz, vedette y modelo argentina. Fue famosa a nivel nacional e internacional por protagonizar numerosas películas sexploitation realizadas por el director Armando Bó entre los años 1959 y 1980.",
+      linkImg: "https://musas.s3.sa-east-1.amazonaws.com/4.png",
+    },
+    {
+      id: 5,
+      nombre: "Leda Valladares y Maria Elena Walsh",
+      descripcion:
+        "Leda Nery Valladares Frías fue una cantante, compositora, literata, poeta, musicóloga y folcloróloga argentina. María Elena Walsh fue una poetisa, escritora, cantautora, dramaturga y compositora argentina, considerada como «mito viviente, prócer cultural y blasón de casi todas las infancias»",
+      linkImg: "https://musas.s3.sa-east-1.amazonaws.com/5.png",
+    },
+    {
+      id: 6,
+      nombre: "Pedro Lembel",
+      descripcion:
+        "Pedro Segundo Mardones Lemebel (Santiago, 21 de noviembre de 1952, 23 de enero de 2015) fue un escritor, cronista y artista plástico chileno.",
+      linkImg: "https://musas.s3.sa-east-1.amazonaws.com/6.png",
+    },
+    {
+      id: 7,
+      nombre: "Marielle Franco",
+      descripcion:
+        "Marielle Francisco da Silva, conocida como Marielle Franco (Río de Janeiro, 27 de julio de 1979 - Ib., 14 de marzo de 2018), fue una socióloga, feminista, política brasileña y militante de los derechos humanos, y concretamente de los derechos de las mujeres negras en Brasil, priorizando el empoderamiento de aquellas mujeres negras que viven en las favelas",
+      linkImg: "https://musas.s3.sa-east-1.amazonaws.com/7.png",
+    },
+    {
+      id: 8,
+      nombre: "Nicanor Parra",
+      descripcion:
+        "Nicanor Segundo Parra Sandoval (San Fabián de Alico, 5 de septiembre de 1914-La Reina, Santiago, 23 de enero de 2018) fue un poeta, profesor, físico e intelectual chileno, cuya obra ha tenido una profunda influencia en la literatura hispanoamericana.",
+      linkImg: "https://musas.s3.sa-east-1.amazonaws.com/8.png",
+    },
+    {
+      id: 9,
+      nombre: "Luis Alberto Spinetta",
+      descripcion:
+        "Luis Alberto Spinetta (Buenos Aires, 23 de enero de 1950, 8 de febrero de 2012) fue un cantante, guitarrista, poeta, escritor y compositor argentino, considerado uno de los más importantes y respetados músicos de Argentina, Latinoamérica y del habla hispana por la complejidad instrumental, lírica y poética de sus obras musicales, tanto en sus múltiples agrupaciones y como solista.",
+      linkImg: "https://musas.s3.sa-east-1.amazonaws.com/9.png",
+    },
+    {
+      id: 10,
+      nombre: "Una Musa",
+      descripcion:
+        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsum blanditiis ut omnis inventore, accusantium iusto totam culpa labore a alias, maiores ipsam, tenetur praesentium perferendis nostrum molestiae quidem sunt pariatur.",
+      linkImg: "https://musas.s3.sa-east-1.amazonaws.com/10.png",
+    },
+    {
+      id: 11,
+      nombre: "Una Musa",
+      descripcion:
+        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsum blanditiis ut omnis inventore, accusantium iusto totam culpa labore a alias, maiores ipsam, tenetur praesentium perferendis nostrum molestiae quidem sunt pariatur.",
+      linkImg: "https://musas.s3.sa-east-1.amazonaws.com/11.png",
+    },
+    {
+      id: 12,
+      nombre: "Una Musa",
+      descripcion:
+        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsum blanditiis ut omnis inventore, accusantium iusto totam culpa labore a alias, maiores ipsam, tenetur praesentium perferendis nostrum molestiae quidem sunt pariatur.",
+      linkImg: "https://musas.s3.sa-east-1.amazonaws.com/12.png",
+    },
+    {
+      id: 13,
+      nombre: "Frida Kahlo",
+      descripcion:
+        "Magdalena Carmen Frida Kahlo Calderón (Coyoacán, Ciudad de México, 6 de julio de 1907-13 de julio de 1954),​ conocida como Frida Kahlo, fue una pintora mexicana. Su obra gira temáticamente en torno a su biografía y a su propio sufrimiento.",
+      linkImg: "https://musas.s3.sa-east-1.amazonaws.com/13.png",
+    },
+    {
+      id: 14,
+      nombre: "Alejandra Pizarnik",
+      descripcion:
+        "Flora Alejandra Pizarnik (Avellaneda, 29 de abril de 1936-Buenos Aires, 25 de septiembre de 1972) fue una poeta, ensayista y traductora argentina. Estudió filosofía y Letras en la Universidad de Buenos Aires y más tarde, pintura con Juan Batlle Planas.",
+      linkImg: "https://musas.s3.sa-east-1.amazonaws.com/14.png",
+    },
+    {
+      id: 15,
+      nombre: "Maria Gabriela Epumer",
+      descripcion:
+        "María Gabriela Epumer (Buenos Aires, 1 de agosto de 1963, 30 de junio de 2003) fue una guitarrista, bajista y cantautora argentina. Paralelamente a su participación en la banda de Charly García, Epumer armó su banda propia llamada A1.",
+      linkImg: "https://musas.s3.sa-east-1.amazonaws.com/15.png",
+    },
+    {
+      id: 16,
+      nombre: "Maria Gabriela Epumer",
+      descripcion:
+        "Laura Ana Merello, más conocida como Tita Merello (Buenos Aires, 11 de octubre de 1904 - Buenos Aires, 24 de diciembre de 2002), fue una actriz y cantante argentina de tango y milonga. Fue una de las primeras cantantes de tango surgidas en la década de 1920 que crearon la modalidad vocal femenina en el rubro.",
+      linkImg: "https://musas.s3.sa-east-1.amazonaws.com/16.png",
+    },
+    {
+      id: 17,
+      nombre: "Violeta Parra",
+      descripcion:
+        "Violeta del Carmen Parra Sandoval (San Fabián de Alico o San Carlos, actual Región de Ñuble, Chile; 4 de octubre de 1917-La Reina, Santiago; 5 de febrero de 1967) fue una artista, música, compositora y cantora chilena, reconocida como una de las principales folcloristas en América del Sur",
+      linkImg: "https://musas.s3.sa-east-1.amazonaws.com/17.png",
+    },
+  ],
+};
